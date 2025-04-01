fe882478d85816b578c1329df3e102a7dbcb9888
pablooarroyo <22476023@live.uem.es>
2025-04-01 11:40:09 +0200

    ping

--- servidor/app.py
+++ servidor/app.py
@@ -18,6 +18,10 @@ if __name__ == "__main__":
     @app.route("/")
     def home():
         return render_template("index.html")
+    
+    @app.route("/ping")
+    def ping():
+        return "¡Servidor funcionando en Railway! 🚀"
 
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
